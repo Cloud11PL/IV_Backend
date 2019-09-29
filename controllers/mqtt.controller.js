cf11148488ac87d01e62c2b51c7ba86036f8233d
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-29 18:18:03 +0200

    Significant code cleanup

--- controllers/mqtt.controller.js
+++ controllers/mqtt.controller.js
@@ -5,109 +5,6 @@ const SingleInput = require('../models/singleInput.model');
 const seriesController = require('./series.controller');
 const deviceController = require('./device.controller');
 
-/*
-New device:
-  DeviceName/start
-    -check if device is added, if not return error
-    -return series number via MQTT
-    -ewentualnie zrobic array z aktywnymi
-
-Existing device:
-
-  DeviceName/start
-    -check if device exists
-    -check the last series for the device
-    -return device series via mqtt
-
-  DeviceName/seriesNumber/payload
-    -find seriesId by seriesNumber and DeviceName
-    -create SingleInput model with payload and save
-
-    PAMIĘTAĆ O RECONNECTED!!!
-
-*/
-
-const newDeviceStartsNoSeries = (packetArray) => {
-  let deviceInfo = {};
-  let seriesInfo = {};
-
-  // Create new device
-  const device = new Device({
-    mqttName: packetArray[0],
-  });
-  device.save().then((data) => {
-    deviceInfo = data;
-    console.log('ADDED DEVICE New device, No series');
-    console.log(data);
-  });
-
-  // Create new series
-  const series = new Series({
-    SeriesId: 0,
-    Device_Id: deviceInfo._id,
-  });
-
-  series.save().then((data) => {
-    console.log('ADDED SERIES New device, No series');
-    console.log(data);
-    seriesInfo = data;
-  });
-};
-
-const existingDeviceNewSeries = (packetArray) => {
-  let existingDevice = {};
-  let maxId = 0;
-  Device.findOne(packetArray[0]).then((data) => {
-    console.log(data);
-    existingDevice = data;
-  });
-  Series.find({
-    Device_Id: existingDevice._id,
-  }).sort('-SeriesId').then((data) => {
-    if (data.SeriesId) {
-      maxId = data.SeriesId;
-    }
-  });
-  // Create new series (higher than the last one)
-  const series = new Series({
-    SeriesId: maxId,
-    Device_Id: existingDevice._id,
-  });
-
-  series.save();
-};
-
-const existingDeviceContinueSeries = (packetArray) => {
-  let existingDevice = {};
-  let maxId = 0;
-
-  // Znajdz urzadzenie po ID
-  Device.findOne(packetArray[0]).then((data) => {
-    console.log(data);
-    existingDevice = data;
-  });
-
-  // Znajdz ostatnia serie
-  Series.find({
-    Device_Id: existingDevice._id,
-  }).sort('-SeriesId').then((data) => {
-    if (data.SeriesId) {
-      maxId = data.SeriesId;
-    }
-  });
-
-  // Stworz input
-  const singleInput = new SingleInput({
-    payload: packetArray[1],
-    seriesId: maxId,
-  });
-
-  singleInput.save().then((data) => {
-    console.log(data);
-  });
-};
-
-// Przerobic na async/await ??
 exports.connectedHandler = topic => new Promise((resolve, reject) => {
   deviceController
     .findDeviceId(topic)
@@ -148,57 +45,20 @@ exports.connectedHandler = topic => new Promise((resolve, reject) => {
     });
 });
 
-exports.handlePacket = (packet) => {
-  // mqtt_name/start
-  const packetArray = packet.split('/');
-  // Urzadzenie zaczyna serie
-  if (packetArray[1] === 'start') {
-    // Urządzenie NIE istnieje w bazie
-    if (!this.findOne(packetArray[0])) {
-      newDeviceStartsNoSeries(packetArray);
-    } else {
-      // Urzadzenie istnieje w bazie
-      existingDeviceNewSeries(packetArray);
-    }
-  } else {
-    // Znane (chyba ze jakis mocny error sie zadzieje i packet zniknie)
-    // Urzadzenie kontynuuje serie
-    existingDeviceContinueSeries(packetArray);
-  }
-  // if (this.findOne(packetArray[0])) {
-  //   // Add pomiar to database
-  //   // Check series
-  // } else {
-  //   // Add new device to db
-  //   // Create new series
+exports.handlePacket = (packet, topic) => new Promise((resolve, reject) => {
+  const newPayload = packet.toString().split('/');
 
-  // }
-  // const packetObject = {
-  //   mqtt_name: packetArray[0],
-  // };
-};
+  const singleInput = new SingleInput({
+    payload: newPayload[1],
+    seriesId: newPayload[0],
+    mqttName: topic,
+  });
 
-
-// exports.createNewDevice = (data) => {
-
-// };
-
-// exports.create = (data) => {
-//   Device.create({
-//     Device_Name: req.body.device_name,
-//     Location: req.body.location,
-//   }, (err) => {
-//     if (err) res(err);
-
-//     Device.findOne({
-//       Device_Name: req.body.device_name,
-//     }, (findErr, result) => {
-//       if (findErr) res(findErr);
-
-//       res(result);
-//     });
-//   });
-// };
+  singleInput.save().then((res) => {
+    resolve(res);
+    console.log(res);
+  });
+});
 
 exports.createDevice = (req, res) => {
   console.log(req.body);
