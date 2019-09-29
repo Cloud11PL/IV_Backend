a3b663fe0e1ae2bf9ee799da9b4807eff2b8e6a8
Wojciech Mista <wojciech.mista@hotmail.com>
2019-09-29 18:44:01 +0200

    Added new routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,8 @@
 const user = require('../controllers/user.controller');
 
 module.exports = (app) => {
+  app.post('/authenticate', user.authenticate);
+  app.post('/authToken', user.authToken);
   app.post('/users', user.create);
   app.get('/users', user.findAll);
   app.get('/users/:userId', user.findOne);
