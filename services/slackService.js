226697b69b78c43ee3058685cbf2b77602a12d93
Wojciech Mista <wojciech.mista@hotmail.com>
2019-10-28 08:44:46 +0100

    Added methods for slack integration

--- services/slackService.js
+++ services/slackService.js
@@ -1,8 +1,12 @@
 const request = require('request');
 
+const { WebClient } = require('@slack/web-api');
 const Device = require('../models/device.model');
+const Series = require('../models/series.model');
 
 const SLACK_TOKEN = require('../secretSlackKey');
+
+const web = new WebClient(SLACK_TOKEN);
 
 exports.getDevices = (req, res) => {
   console.log(req.body);
@@ -34,5 +38,90 @@ exports.getDevices = (req, res) => {
       }
       res.json();
     });
+  });
+};
+
+exports.showActiveDevices = async () => {
+  const activeDevices = await Device.find({
+    Active: true,
+  });
+
+  const lastSeries = await activeDevices.forEach((device) => {
+    Series.findOne({
+      Device_Id: device._id,
+    });
+  });
+  let index = 0;
+  const blocks = lastSeries.forEach((series) => {
+    index += 1;
+    return {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Device ${activeDevices[index - 1].Device_Name} with level at TODO ${series.SeriesId}`,
+      },
+    };
+  });
+
+  web.chat.postMessage({
+    channel: '#general',
+    text: 'Active devices',
+    blocks: [
+      blocks(),
+    ],
+  });
+};
+
+exports.sendLowLevelAlert = async (deviceName) => {
+  const deviceData = await Device.find({
+    mqttName: deviceName,
+  });
+
+  web.chat.postMessage({
+    channel: '#general',
+    text: `<!here> Low level at device ${deviceData[0].Device_Name}`,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `⚠ Low level at device ${deviceData[0].Device_Name} ⚠`,
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*Device name:* ${deviceData[0].Device_Name}\n*Device ID*: ${deviceName}\n*Location*: Bed ${deviceData[0].Location}`,
+        },
+      },
+    ],
+  });
+};
+
+exports.deviceDisconnected = async (deviceName) => {
+  const deviceData = await Device.find({
+    mqttName: deviceName,
+  });
+
+  web.chat.postMessage({
+    channel: '#general',
+    text: 'Device was disconnected',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: '⚠️ Device has been disconnected. 🤷‍♀️',
+        },
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*Device name:* ${deviceData[0].Device_Name}\n*Device ID*: ${deviceName}\n*Location*: Bed ${deviceData[0].Location}`,
+        },
+      },
+    ],
   });
 };
